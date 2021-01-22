c189043625da3ecbe22387e65e7368c13d3b5f51
cooperbrislain <cbrislain@gmail.com>
2021-01-21 21:32:03 -0800

    there we go, uses css style

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 window.onload = () => {
-    document.getElementById('container').innerHTML = recurse_poster(1024, 768);
+    document.getElementById('container').innerHTML = recurse_poster(80, 1024, 25);
 }
 
-function recurse_poster(x, y, factor= 0.8, small=25) {
-    let [x_, y_, fontsize] = [x*factor, y*factor, factor*100];
-    const inner = x_<=small||y_<=small? '' : recurse_poster(x_, y_, factor);
+function recurse_poster(factor=80, x=1024, small=25) {
+    console.log(x*(factor/100));
+    if (x*(factor/100) <= small) return '';
+    const inner = recurse_poster(factor, x*(factor/100), small);
     return `
-        <div class="poster" style="width: ${x_}; height: ${y_}; font-size: ${fontsize}%">
+        <div class="poster" style=" transform: scale(${factor/100}, ${factor/100})">
             <div class="inner">
                 ${inner}
             </div>
